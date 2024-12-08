1aab7f299348605cdf54bae7a5e2bd318933b9ae
three jin <1186340002@qq.com>
2024-12-08 19:40:03 +0800

    2024/12/8更新

---
+++ zhouThreeJin/algorithm/01.go
@@ -0,0 +1,39 @@
+import "math"
+
+func thirdMax(nums []int) int {
+	var max int
+	key := 1
+	max = nums[0]
+	max2 := math.MinInt
+	max3 := math.MinInt
+	for _, v := range nums {
+		if v >= max {
+			if v == max {
+				continue
+			}
+			max3 = max2
+			max2 = max
+			max = v
+			key++
+			continue
+		} else if v >= max2 {
+			if v == max2 {
+				continue
+			}
+			max3 = max2
+			max2 = v
+			key++
+			continue
+		} else if v >= max3 {
+			if v == max3 {
+				continue
+			}
+			max3 = v
+			key++
+		}
+	}
+	if key >= 3 {
+		return max3
+	}
+	return max
+}
